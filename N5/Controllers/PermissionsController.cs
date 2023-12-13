0c5d9665c77e574a31f50d193e37bf6e9b560797
Ezequiel Dutra <squiel.dutra@gmail.com>
2023-12-12 23:11:57 -0300

    Se agrega logger a los métodos.

--- N5/Controllers/PermissionsController.cs
+++ N5/Controllers/PermissionsController.cs
@@ -12,21 +12,26 @@ namespace N5.Controllers
     public class PermissionsController : ControllerBase
     {
         private readonly IMediator _mediator;
-        public PermissionsController(IMediator mediator)
+        private readonly ILogger<PermissionsController> _logger;
+        public PermissionsController(IMediator mediator, ILogger<PermissionsController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IEnumerable<PermissionDto>> GetAll()
         {
-            return await _mediator.Send(new GetAllPermissionsQuery());
+            _logger.LogInformation("Ejecutando GetAllPermissionsQuery...");
+            var permissions = await _mediator.Send(new GetAllPermissionsQuery());
+            _logger.LogInformation("Se ejecutó con éxito GetAllPermissionsQuery.");
+            return permissions;
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, UpdatePermissionCommand command)
         {
-            Console.Write(command);
+            _logger.LogInformation("Ejecutando UpdatePermissionCommand...");
             if (id != command.Id)
             {
                 return BadRequest();
@@ -37,13 +42,16 @@ namespace N5.Controllers
             {
                 return NotFound();
             }
+            _logger.LogInformation("Se ejecutó con éxito UpdatePermissionCommand.");
             return NoContent();
         }
 
         [HttpPost]
         public async Task<ActionResult<PermissionDto>> Create(CreatePermissionCommand command)
         {
+            _logger.LogInformation("Ejecutando CreatePermissionCommand...");
             var permission = await _mediator.Send(command);
+            _logger.LogInformation("Se ejecutó con éxito CreatePermissionCommand...");
             return permission;
         }
     }
--- N5/Program.cs
+++ N5/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using N5.Infrastructure;
 using N5.Infrastructure.Repositories;
 using N5.Infrastructure.UnitOfWork;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +18,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Permissions"));
 
+//Repository + Unit of Work
 builder.Services.AddScoped<IPermissionRepository, PermissionRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+//Logger Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
 var app = builder.Build();
 
